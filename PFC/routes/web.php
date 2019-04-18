43c8d6d64b55a19eefee16208f8988a902abcd0d
leascherf <leascherf@gmail.com>
2019-04-18 20:30:50 -0300

    08 - Curso de Laravel 5.6, Rutas
    creacion de las dos primeras rutas en laravel

--- PFC/routes/web.php
+++ PFC/routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Video 08
+//Creacion de las primeras rutas
+Route::get('/mi_primer_ruta', function(){
+return ('Hola barats');
+});
+//Rutas que recibe parametros nombre y lastname
+//En caso de que lastname venga vacio el parametro le doy un valor por defecto
+Route::get('/name/{name}/lastname/{lastname}', function($name, $lastname ='apellido'){
+return ('hola soy' .$name .$lastname);
+}); 
