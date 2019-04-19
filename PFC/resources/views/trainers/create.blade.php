22b2caf7da2d559b6049ec340fd454f9d66ceb95
leascherf <leascherf@gmail.com>
2019-04-19 12:46:40 -0300

    25 - Curso de Laravel 5.6, Sub Views Podemos utilizar subvistas simplemente llamandolas con el metodo @include('nombre vista')

--- PFC/resources/views/trainers/create.blade.php
+++ PFC/resources/views/trainers/create.blade.php
@@ -5,14 +5,7 @@
 @section('content')
 <form  class="form-group" method="POST" action="/trainers" enctype="multipart/form-data">
 	@csrf
-	<div class="form-group">
-			<label for="">Nombre</label>
-			<input type="text" name="name" class="form-control"></input>
-	</div>
-	<div class="form-group">
-			<label for="">Avatar</label>
-			<input type="file" name="avatar"></input>			
-	</div>
+			@include('trainers.form')
 			<button type="submit" class="btn btn-primary mt-3">Guardar</button>
 </form>
 @endsection	
---
+++ PFC/resources/views/trainers/form.blade.php
@@ -0,0 +1,8 @@
+<div class="form-group">
+			<label for="">Nombre</label>
+			<input type="text" name="name" class="form-control"></input>
+	</div>
+	<div class="form-group">
+			<label for="">Avatar</label>
+			<input type="file" name="avatar"></input>			
+	</div>
