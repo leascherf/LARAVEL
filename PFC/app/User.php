5d9fc8080adb409a3e4472b318db428d11c497b0
leascherf <leascherf@gmail.com>
2019-04-19 15:13:53 -0300

    36 - Curso de Laravel 5.6, Roles de usuario - Lógica de validación En user.php va toda la logica para la validacion de roles
    Voy a crear tres funciones para el manejor de las mismas
    
    Una funcion sera la basica para retornar si el user tiene o no un rol
    otra donde mirare si tiene un solo rol o si es un array con roles
    Y la ultima es una capa para  autorizar el rol controla si tiene el rol o si no lanza un abort con su codigo de error y su mensaje

--- PFC/app/User.php
+++ PFC/app/User.php
@@ -14,6 +14,48 @@ class User extends Authenticatable
 public function roles(){
     return $this->belongsToMany('PFC\Role');
 }
+
+//nos falta una capa de interaccion mas directa
+//este sera el que desencadene todo el proceso
+public funcion authorizeRoles($roles){
+        if ($this->hasAnyRole($roles)){
+                return true;
+        }
+        //si no se cumple tendremos que decirle al usuario que algo anda mal entonces uso ABORT() y genero una excepcion http
+        abort(401,'Esta accion no esta autirozada');
+}
+
+//creo una funcion para ver si tiene algun rol ya que tiene mas un rol el usuario
+public function hasAnyRole($roles){
+    //lo trataremos de dos formas ya que puedo recibir un arreglo de roles o solo puedo recibir un arreglo
+    //si es un array hacemos esto
+    if (is_array($roles)){
+        foreach ($roles as $role) {
+           if($this->hasRole($role)){
+            return true;
+             }  
+        }
+    //de lo contrario 
+    }else {
+        //llamamos a la funcion que tenemos abajo y retornamos un true
+        if($this->hasRole($roles)){
+            return true;
+        }
+    }
+    //y en caso de que no tenga nada retorno un falso
+    return false;
+}
+
+//funcion para validar si el usuario tiene este rol
+public function hasRole($role){
+    //un usuario puede tener mas de un rol
+    //la validacion para ver si el usario tiene ese rol asignado
+    //si en este modelo actual seria el usuario dentro de su relacion con roles donde le pasamos su nombre si existe me mande el primero que encuentre
+    if ($this->roles()->where('name', $role->first())){
+        return true;
+    }
+         return false;
+}
     /**
      * The attributes that are mass assignable.
      *
