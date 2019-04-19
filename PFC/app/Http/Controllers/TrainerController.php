2b8a3487441d79649f696a60c4796188809080f3
leascherf <leascherf@gmail.com>
2019-04-19 11:47:55 -0300

    Video 22: customizar el implicit binding podemos customizar con que atributo podemos hacer el implicit binding y lo modificamos por el slug Lo agregamos al modelo trainer!!!
    Al hacer esto nos permite poder modificar en el show el parametro ($slug) por el de (Trainer $trainer)

--- PFC/app/Http/Controllers/TrainerController.php
+++ PFC/app/Http/Controllers/TrainerController.php
@@ -67,9 +67,9 @@ class TrainerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($slug)
+    public function show(Trainer $trainer)
     {
-        $trainer= Trainer::where('slug','=',$slug)->firstOrFail();
+
     
      return view('trainers.show', compact('trainer'));
 
